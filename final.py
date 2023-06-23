8a2372d2e50a6cf974ec03f1e340d092940a799e
ekffuf <ekffuf@naver.com>
2023-06-23 20:14:10 +0900

    ekffuf_flask_final_test_dodo

--- final.py
+++ final.py
@@ -12,49 +12,54 @@ from keras.models import load_model
 
 # m4a를 wav파일로 변환
 def m4a_wav_convert(path):
-    encoded_path = urllib.parse.quote("쇼핑_462.m4a")
-    wav_path = AudioSegment.from_file(encoded_path, format="m4a", encoding="utf-8")
-    wav_path.export("쇼핑_462.wav", format="wav")
+    encoded_path = urllib.parse.unquote(path)
+    m4a_file = AudioSegment.from_file(encoded_path, format="m4a", encoding="utf-8")
+    wav_path = m4a_file.export(path.replace(".m4a", ".wav"), format="wav")
     return wav_path
 
 
 def stt(wav):
     # 파일로부터 음성 불러오기, STT변환
     r = sr.Recognizer()
-    with sr.AudioFile(m4a_wav_convert) as source:
-        audio = r.record(source)
-    r_text = r.recognize_google(audio, language='ko')
-    return r_text
+    real = ""
+    # if문을 통해서 총음원 길이가 120,000밀리세컨드를 넘을 경우
+    # for문을 돌면서 120,000밀리세컨드로 쪼개서 변환시키기
+    # 변환된 파일들을 for문을 이용해서 합치기(for문이 끝나기 전에)
+    for wav in range():
+        with sr.AudioFile(m4a_wav_convert(wav)) as source:
+            audio = r.record(source)
+        r_text = r.recognize_google(audio, language='ko')
+        real += r_text
+    return real
 
 
 # 모델 호출
 # --pre방식
 with open("tokenizer_pre.pickle", "rb") as f:
     tokenizer1 = pk.load(f)
-# with open("model1.pickle", "rb") as f:
-#     model1 = pk.load(f)
 model1 = load_model("model_pre.h5")
 
 # --post방식
 with open("tokenizer_post.pickle", "rb") as f:
     tokenizer2 = pk.load(f)
-# with open("model2.pickle", "rb") as f:
-#     model2 = pk.load(f)
 model2 = load_model("model_post.h5")
 
 
 # 새로운 음성 판별
 def predict(wav_path):
     string = stt(wav_path)
-    real_sequences1 = tokenizer1.texts_to_sequences(string)
+    string = " ".join(string)
+    real_sequences1 = tokenizer1.texts_to_sequences([string])
     real_seq1 = pad_sequences(real_sequences1, maxlen=1000, truncating="pre")
     result1 = model1.predict(real_seq1)
+    print(result1)
 
-    real_sequences2 = tokenizer2.texts_to_sequences(string)
+    real_sequences2 = tokenizer2.texts_to_sequences([string])
     real_seq2 = pad_sequences(real_sequences2, maxlen=1000, truncating="post")
     result2 = model2.predict(real_seq2)
+    print(result2)
 
-    if result1 or result2 >= 0.35:
+    if (result1 >= 0.35) or (result2 >= 0.35):
         detect = 1
     else:
         detect = 0
@@ -65,14 +70,15 @@ app = Flask(__name__)
 api = Api(app)
 
 
-@api.route("/fraud/filename/<string:m4a_path>")
+@api.route("/fraud/filename/<string:m4apath>")
 class HelloWorld(Resource):
-    def get(self, m4a_path):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        print(os.getcwd())
-        wav_path = m4a_wav_convert(m4a_path)
+    def get(self, m4apath):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+        m4apath = "./203.m4a"
+        wav_path = m4a_wav_convert(m4apath)
         detect = predict(wav_path)
         return {"result": detect}
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    # app.run(debug=True, host='182.229.34.184', port=9966)
+    app.run(debug=True, port=9966)
